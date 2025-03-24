83c0f073c688209ffb02048c89986c9b9aada7d0
annabellekiefer <annabellemel@web.de>
2025-03-24 11:18:31 +0100

    Adding Python script

--- a_python_script.py
+++ a_python_script.py
@@ -1,1 +1,11 @@
 import os
+import sys
+
+#Hands-on 24.03.2025
+
+print(sys.path)
+help("modules")
+days_of_the_week = 7
+def say_hello(recipient):
+    print("Hello, world! Hello {} ".format(recipient))
+    return recipient
